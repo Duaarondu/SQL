263e35596ab513159fcb7460b64ca567a043247c
Aaron Du <151097536+Duaarondu@users.noreply.github.com>
2025-11-04 16:17:38 -0500

    Connection from SQL to IntelliJ

--- src/com/example/dbapp/MainMenu.java
+++ src/com/example/dbapp/MainMenu.java
@@ -1,8 +1,10 @@
 package com.example.dbapp;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import static com.example.dbapp.VariousQueriesAndScanner.setupClosingDBConnection;
 
 public class MainMenu {
-
+    public static void main(String[] args) {
+        Database.connect();
+        setupClosingDBConnection();
+    }
 }
